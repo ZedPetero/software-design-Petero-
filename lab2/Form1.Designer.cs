adc70c1ef594aaad7375c792750c7ea57242fbad
CpELaboratory 216 <CpELaboratory 216@BE216-PC36>
2026-01-28 08:47:56 +0800

    Week 2 Exercies Task 2

--- lab2/Form1.Designer.cs
+++ lab2/Form1.Designer.cs
@@ -20,20 +20,65 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(207, 96);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(386, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(344, 169);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Solve Factorial";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(381, 233);
+            label1.Name = "label1";
+            label1.RightToLeft = RightToLeft.No;
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Result";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Label label1;
+    }
 }
--- lab2/Form1.cs
+++ lab2/Form1.cs
@@ -1,10 +1,23 @@
 namespace lab2
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int RecursiveFactorial(int n)
+        {
+            if (n == 1) return 1;
+            return n * RecursiveFactorial(n - 1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(textBox1.Text);
+            int result = RecursiveFactorial(number);
+            label1.Text = $"Factorial : {result}";
+        }
+    }
 }
